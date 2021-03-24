8d4f0a7005dfb09f3ccfd11091a7642bd764661c
github username <60378440+vmar13@users.noreply.github.com>
2021-03-23 22:43:33 -0400

    added populateDrugCards func

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    const cardContainer = document.querySelector('.container')
+
     const getSearchResults = () => {
         fetch('https://api.fda.gov/drug/event.json?search=receivedate:[20040101+TO+20081231]&limit=1')
         .then(res => res.json())
-        .then(queryResults => console.log(queryResults))
+        .then(populateDrugCards)
     }
 
     // let div = document.createElement('div')
     // div.innerHTML('MEDY')
 
+    const populateDrugCards = (results) => {
+        cardContainer.innerHTML = ''
+
+        results.forEach(result => {
+
+        const resultDiv = document.createElement('div')
+        })
+    }
+
+     
+      
+
     getSearchResults()
+    populateDrugCards()
 })
