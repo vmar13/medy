ff2fc75b25d9c02db23ed13b0560539feebd8cb3
github username <60378440+vmar13@users.noreply.github.com>
2021-03-23 22:38:54 -0400

    queried fda api for search results

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
+
+    const getSearchResults = () => {
+        fetch('https://api.fda.gov/drug/event.json?search=receivedate:[20040101+TO+20081231]&limit=1')
+        .then(res => res.json())
+        .then(queryResults => console.log(queryResults))
+    }
+
+    // let div = document.createElement('div')
+    // div.innerHTML('MEDY')
+
+    getSearchResults()
 })
